e5d9ce070d2a8d05c40c6c49c25070bc1f7e3d53
P3DDRO1 <107056469+P3DDRO1@users.noreply.github.com>
2024-07-05 08:20:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const botoes = document.querySelectorAll(botao);
+const textos = document.querySelectorAll(aba-conteudo);
+const contadores = document.querySelectorAll(contador);
+const tempoObjetivo1 = new Date(2024-10-05T00:00:00);
+let tempoAtual = new Date();
+contadores[0].textContent = "Contagem regressiva";
+contadores[0].textContent = tempoObjetivo1;
 for (let i=0; i < botoes.length; i++) { console.log(i); }
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
@@ -7,4 +13,6 @@ for(let i=0;i <botoes.length;i++){
 for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove(ativo);
 }
 botoes[i].classList.add(ativo);
+textos[i].classList.add(ativo);
+textos[j].classList.remove(ativo);
 }
