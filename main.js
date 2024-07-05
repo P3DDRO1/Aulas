9b03f0ae1b9038fa1a464b48545096a33831446a
P3DDRO1 <107056469+P3DDRO1@users.noreply.github.com>
2024-07-05 11:48:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const botoes = document.querySelectorAll(botao);
-const textos = document.querySelectorAll(aba-conteudo);
-const contadores = document.querySelectorAll(contador);
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
@@ -14,18 +14,18 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + dias + horas + horas + minutos + minutos + segundos + segundos;
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
 contadores[0].textContent = "Contagem regressiva";
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 for (let i=0; i < botoes.length; i++) { console.log(i); }
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
-    botoes[i].classList.add(ativo);
+    botoes[i].classList.add("ativo");
 }
-for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove(ativo);
+for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove("ativo");
 }
-botoes[i].classList.add(ativo);
-textos[i].classList.add(ativo);
-textos[j].classList.remove(ativo);
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+textos[j].classList.remove("ativo");
 }
