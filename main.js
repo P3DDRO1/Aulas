e76c098d8069ed5fe2f8cbf50629b2e3444822b8
P3DDRO1 <107056469+P3DDRO1@users.noreply.github.com>
2024-08-09 11:46:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -41,16 +41,23 @@ function calculaTempo(tempoObjetivo) {
 
     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
     if (tempoFinal > 0){
-        return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
-        }
-    else {
-        return "Prazo Finalizado";
+        return [dias,horas,minutos,segundos];
+        } else {
+        return [0,0,0,0];
         }
 }
 
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
+    document.getElementById("dias"+i).textContent =
+    calculaTempo(tempos[i])[0];
+    document.getElementById("horas"+i).textContent =
+    calculaTempo(tempos[i])[1];
+    document.getElementById("min"+i).textContent =
+    calculaTempo(tempos[i])[2];
+    document.getElementById("seg"+i).textContent =
+    calculaTempo(tempos[i])[3];
+    console.log("seg"+i);
     }
     }
     function comecaCronometro(){
